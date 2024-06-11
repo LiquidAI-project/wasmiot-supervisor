d39eafc30910c95b41b959cbd11a8b193325e9aa
Teemu Autto <teemu@rautakuu.org>
2024-06-11 19:57:55 +0000

    use dotenv to load environment settings.

--- host_app/__main__.py
+++ host_app/__main__.py
@@ -1,14 +1,6 @@
 #from .app import create_app, teardown_zeroconf
 import os
 
-# Have to setup environment variables before importing flask app
-os.environ.setdefault("FLASK_APP", "host_app")
-os.environ.setdefault("FLASK_ENV", "development")
-os.environ.setdefault("FLASK_DEBUG", "1")
-
-# Set wasmiot-orchestrator logging endpoint to send logs to the orchestrator. ex-http://172.21.0.3:3000/device/logs
-os.environ.setdefault("WASMIOT_LOGGING_ENDPOINT", "")
-
 from .utils.configuration import INSTANCE_PATH
 
 from host_app.flask_app import app as flask_app
@@ -16,7 +8,16 @@ from host_app.flask_app import app as flask_app
 if __name__ == "__main__":
     print("Starting program")
 
-    debug = bool(os.environ.get("FLASK_DEBUG", 0))
+    import dotenv
+    dotenv.load_dotenv()
+
+    # Have to setup environment variables before importing flask app
+    os.environ.setdefault("FLASK_APP", "host_app")
+    os.environ.setdefault("FLASK_ENV", "development")
+    os.environ.setdefault("FLASK_DEBUG", "1")
+    # Set wasmiot-orchestrator logging endpoint to send logs to the orchestrator. ex-http://172.21.0.3:3000/device/logs
+    os.environ.setdefault("WASMIOT_LOGGING_ENDPOINT", "")
+
 
     #print('starting modules')
     #wasm_daemon = threading.Thread(name='wasm_daemon',
@@ -24,6 +25,9 @@ if __name__ == "__main__":
     #                               target=wa.start_modules,
     #                                 )
     #wasm_daemon.start()
+
+    debug = bool(os.environ.get("FLASK_DEBUG", 0))
+
 
     app = flask_app.create_app(instance_path=INSTANCE_PATH)
 
